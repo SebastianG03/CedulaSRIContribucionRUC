27e3182160ecbc0b6e111d836286708617c887fa
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2024-06-03 19:38:18 -0500

    Se arregla un problema de comunicación

--- src/main/java/org/example/validatesricontributors/Response.java
+++ src/main/java/org/example/validatesricontributors/Response.java
@@ -1,13 +1,9 @@
 package org.example.validatesricontributors;
 
-
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 public class Response {
-    public Boolean isContributor;
+    public Boolean IsContributor;
 
     public Response(Boolean isContributor) {
-        this.isContributor = isContributor;
+        this.IsContributor = isContributor;
     }
 }
